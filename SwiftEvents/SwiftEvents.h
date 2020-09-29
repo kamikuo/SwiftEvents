e3fb0d84a3cf8b1ee5f46df1c8f3a5380a33f05e
kamikuo <kamijiawei@gmail.com>
2020-09-29 14:38:28 +0800

    new project

---
+++ SwiftEvents/SwiftEvents.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEvents.h
+//  SwiftEvents
+//
+//  Created by kamikuo on 2020/9/28.
+//  Copyright © 2020年 kamikuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftEvents.
+FOUNDATION_EXPORT double SwiftEventsVersionNumber;
+
+//! Project version string for SwiftEvents.
+FOUNDATION_EXPORT const unsigned char SwiftEventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEvents/PublicHeader.h>
+
+
